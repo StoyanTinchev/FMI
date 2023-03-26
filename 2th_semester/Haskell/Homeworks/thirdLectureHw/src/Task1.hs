401cbcb8dfcdd343c59061f3d4a11aaf57aebdbf
StoyanTinchev <st8tinchev@gmail.com>
2023-03-26 23:34:17 +0300

    Edit: run .githook pre-commit script for the haskell test file

--- 2th_semester/Haskell/Homeworks/thirdLectureHw/src/Task1.hs
+++ 2th_semester/Haskell/Homeworks/thirdLectureHw/src/Task1.hs
@@ -1,11 +1,11 @@
 module Task1 (removeFistOccurrence) where
 
---Define a function that removes the first occurrence of a digit in a number by going from right to left.
---For example, removeFistOccurrence 110 1 == 10
+-- Define a function that removes the first occurrence of a digit in a number by going from right to left.
+-- For example, removeFistOccurrence 110 1 == 10
 removeFistOccurrence :: Int -> Int -> Int
 removeFistOccurrence num occ = helper num 0 0
   where
     helper :: Int -> Int -> Int -> Int
     helper curr pastNum iter
-      | mod curr 10 == occ = 10 ^ iter * div curr 10 + pastNum
-      | otherwise = helper (div curr 10) (10 ^ iter * mod curr 10 + pastNum) (iter + 1)
+      | mod curr 10 == occ = 10^iter * div curr 10 + pastNum
+      | otherwise = helper (div curr 10) (10^iter * mod curr 10 + pastNum) (iter + 1)
--- 2th_semester/Haskell/Homeworks/thirdLectureHw/src/Task3.hs
+++ 2th_semester/Haskell/Homeworks/thirdLectureHw/src/Task3.hs
@@ -1,20 +1,20 @@
 module Task3 (calcSeriesSum) where
 
---Define a function that accepts two real numbers and calculates the n-th partial sum from the following sequence:
+-- Define a function that accepts two real numbers and calculates the n-th partial sum from the following sequence:
 calcSeriesSum :: Double -> Int -> Double
 calcSeriesSum x n = helper 1 1
   where
     helper :: Int -> Int -> Double
     helper count prevDivider
       | count > n =
-        ((-1) ^ count * 2 ^ count * x ^ (count - 1))
-          / fromIntegral (prevDivider * (2 * count - 1))
+          ((-1)^count * 2^count * x^(count - 1))
+            / fromIntegral (prevDivider * (2 * count - 1))
       | otherwise =
-        ((-1) ^ count * (2 ^ count * x ^ (count - 1)))
-          / fromIntegral (prevDivider * (2 * count - 1))
-          + helper (count + 1) (prevDivider * (2 * count - 1))
+          ((-1)^count * (2^count * x^(count - 1)))
+            / fromIntegral (prevDivider * (2 * count - 1))
+            + helper (count + 1) (prevDivider * (2 * count - 1))
 
---print $
---    ((-1) ^ 1 * 2 ^ 1 * 1 ^ 0) / (1 * (2 * 1 - 1)) -- = -2
---  + ((-1) ^ 2 * 2 ^ 2 * 1 ^ 1) / (1 * (2 * 2 - 1)) -- = 1.3333333333333333
---  + ((-1) ^ 3 * 2 ^ 3 * 1 ^ 2) / (3 * (2 * 3 - 1)) -- = -0.5333333333333333
+-- print $
+--    ((-1)^1 * 2^1 * 1^0) / (1 * (2 * 1 - 1)) -- = -2
+--  + ((-1)^2 * 2^2 * 1^1) / (1 * (2 * 2 - 1)) -- = 1.3333333333333333
+--  + ((-1)^3 * 2^3 * 1^2) / (3 * (2 * 3 - 1)) -- = -0.5333333333333333
